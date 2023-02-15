79b91c5a908838f249678cf698895176a8fe4ae9
derNif <107477879+derNif@users.noreply.github.com>
2023-02-15 15:25:13 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -78,7 +78,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch the data from server = bot's response
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-3ujq.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
